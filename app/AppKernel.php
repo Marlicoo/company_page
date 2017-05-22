3fdabe38d61fba7ac533ee9b9eebf9c2e64c5262
Marlicoo <marlena.zdanowicz@gmail.com>
2017-05-22 20:53:56 +0200

    first integration with FOS and SonataUser

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,13 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+            new AdminBundle\AdminBundle(),
+            new Sonata\CoreBundle\SonataCoreBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle(),
+            new Sonata\UserBundle\SonataUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
--- src/AdminBundle/AdminBundle.php
+++ src/AdminBundle/AdminBundle.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace UserBundle;
+namespace AdminBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class UserBundle extends Bundle
+class AdminBundle extends Bundle
 {
 }
